203604c4d48b2858ef6900b27d724ed41640a0c7
CraigCODA <268316751+craigCODA@users.noreply.github.com>
2026-09-15 21:55:20 -0500

    feat(net): add bounded Ethernet-II parser

---
+++ user/probes/link-layer/src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+
+#[cfg(not(test))]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo<'_>) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[cfg(test)]
+fn main() {}
