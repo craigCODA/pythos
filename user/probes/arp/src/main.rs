0748ed95ad1bda6463b1bb7438997bb9194832d7
CraigCODA <268316751+craigCODA@users.noreply.github.com>
2026-09-16 19:36:13 -0500

    feat(net): add bounded ARP codec

---
+++ user/probes/arp/src/main.rs
@@ -0,0 +1,24 @@
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+
+#[cfg(not(test))]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo<'_>) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[cfg(test)]
+fn main() {}
